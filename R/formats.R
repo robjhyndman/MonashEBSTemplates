efca8aa058b0a021492bb2436821c1b252bc8c96
Rob J Hyndman <robjhyndman@gmail.com>
2017-10-24 22:36:40 +1100

    Added exam and letter output formats

---
+++ R/formats.R
@@ -0,0 +1,29 @@
+#' Output formats for Monash EBS documents
+#'
+#' Each function is a wrapper for \code{\link[bookdown]{pdf_document2}} to
+#' produce documents in Monash EBS style.
+#'
+#' @param \dots Arguments passed to \code{\link[bookdown]{pdf_document2}}.
+#'
+#' @return An R Markdown output format object.
+#'
+#' @author Rob J Hyndman
+#'
+#' @export
+letter <- function(...) {
+  template <- system.file("rmarkdown/templates/Letter/resources/monashletter.tex",
+                          package="MonashEBSTemplates")
+   bookdown::pdf_document2(...,
+     template = template
+   )
+}
+
+#' @rdname letter
+#' @export
+exam <- function(...) {
+  template <- system.file("rmarkdown/templates/Exam/resources/examtemplate.tex",
+                          package="MonashEBSTemplates")
+  bookdown::pdf_document2(...,
+                          template = template
+  )
+}
